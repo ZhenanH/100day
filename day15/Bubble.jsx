c8037ea8dc17b3879657255cb58cdb34e8e7d103
Zhenan Hong <guoer23@gmail.com>
2017-10-22 19:02:18 -0400

    d15

--- day15/Bubble.jsx
+++ day15/Bubble.jsx
@@ -10,102 +10,42 @@ window.Bubble = class Bubble extends React.Component {
 
 
 
-    this.canvas.width = Number(this.getElementStyle(this.outterDiv,"width").slice(0,-2));
-    this.canvas.height = Number(this.getElementStyle(this.outterDiv,"height").slice(0,-2));
-    this.canvas.style.filter = "url('#liquid')";
-    this.ctx = this.canvas.getContext("2d");
-    this.ctx.webkitImageSmoothingEnabled=true;
+      this.mainBall.style.width =  Number(this.getElementStyle(this.outterDiv,"width").slice(0,-2))/2 +"px";
+      this.mainBall.style.height = Number(this.getElementStyle(this.outterDiv,"height").slice(0,-2))/2 + "px";
+      this.mainBall.classList += "bigBall";
 
-    this.bigBubble();
-    this.smallBubbles = this.generateSmallBubble(20);
-    // this.ctx.fillStyle = "black";
-    // this.ctx.fillRect(0,0,this.canvas.width,this.canvas.height);
+      this.smallBubbles = document.querySelectorAll('.smallBubbles');
 
-   // window.addEventListener('resize',this.resizeCanvas.bind(this));
+      var r = 0.8*Number(this.getElementStyle(this.outterDiv,"width").slice(0,-2))/2;
+      this.smallBubbles.forEach(function(s){
 
-    //img
+        var theta = Math.random()*2*Math.PI;
+        var duration = (Math.random()+1)*1500;
+        var flyout = s.animate([
+          {transform:"translate(0,0)",opacity:1},
+           {transform:"translate("+Math.cos(theta)*r+"px,"+Math.sin(theta)*r+"px)",opacity:0.5}
+          ],duration);
+
+        flyout.onfinish = function(){
+          flyout.play();
+        };
 
 
-    this.draw();
+
+        });
+
+
 
 
   }
 
 
   componentWillUnmount() {
-      window.removeEventListener('resize',this.resizeCanvas.bind(this));
-    }
+     
 
 
-
-  redrawContext(){
-    if(this.ctx){
-      //this.ctx.fillStyle = 'white';
-      this.ctx.clearRect(0,0,this.canvas.width,this.canvas.height);
-    }
-  }
-
-  bigBubble(){
-
-    this.ctx.beginPath();
-    this.ctx.arc(this.canvas.width/2,this.canvas.height/2, 100 ,0, 2*Math.PI);
-    this.R = Math.min(this.canvas.width/2,this.canvas.height/2);
-    this.ctx.fillStyle = "rgb(66, 244, 179)";
-    this.ctx.fill();
-  }
-
-  smallBubble(x,y,r){
-    this.ctx.beginPath();
-    this.ctx.arc(x,y,r,0, 2*Math.PI);
-    this.ctx.fillStyle = "rgb(66, 244, 179)";
-    this.ctx.fill();
-  }
-
-  generateSmallBubble(count){
-    var bubbles = [];
-    for(var i=0;i<count;i++){
-      var bubble = {};
-      bubble.x = this.canvas.width/2;
-      bubble.y = this.canvas.height/2;
-      bubble.theta = Math.random()*2*Math.PI;
-      var v = (Math.random()-1)*2;
-      bubble.vx = Math.cos(bubble.theta)*v;
-      bubble.vy = Math.sin(bubble.theta)*v;
-      bubble.r = Math.random()*30;
-      bubbles.push(bubble);
-    }
-
-    return bubbles;
-  }
-
-  draw(){
-
-      this.redrawContext();
-      this.bigBubble();
-      for(var i=0;i<this.smallBubbles.length;i++){
-          var b = this.smallBubbles[i];
-          this.smallBubble(b.x,b.y,b.r);
-
-          // b.vx += 0.01;
-          // b.vy += 0.01;
-          b.x += b.vx*4;
-          b.y += b.vy*4;
-
-          //console.log("sides: "+Math.pow(Math.cos(b.x),2) + Math.pow(Math.sin(b.y),2));
-          //console.log("r: "+ Math.abs(Math.cos(b.theta)*b.y));
-          if(Math.abs(Math.cos(b.theta)*b.y) >= this.R*1.5 ) {
-
-            b.x = this.canvas.width/2;
-            b.y =  this.canvas.height/2;
-          }
-
-      }
-    
-
-      requestAnimationFrame(this.draw.bind(this));
 
   }
-
 
 
   getElementStyle(ele,attribute){
@@ -113,22 +53,20 @@ window.Bubble = class Bubble extends React.Component {
     return style;
   }
 
-  randn_bm() {
-    var u = 0, v = 0;
-    while(u === 0) u = Math.random(); //Converting [0,1) to (0,1)
-    while(v === 0) v = Math.random();
-    return Math.sqrt( -2.0 * Math.log( u ) ) * Math.cos( 2.0 * Math.PI * v );
-}
-
-
-
 
   render(){
 
+    var count = 20;
+    var smallBubbles = [];
+    for(var i=0;i<count;i++){
+      var r = 20*(Math.random()+1);
+      smallBubbles.push(<div key = {i} className="smallBubbles" style={{position:"absolute", width:r+"px",height:r+"px", backgroundColor:"rgb(65, 244, 217",borderRadius:"50%"}}></div>);
+    }
 
     return (
-      <div ref={(div)=>{this.outterDiv = div;}} className = {this.props.className}>
-        <canvas ref={(div)=>{this.canvas = div;}} width={this.props.width} height={this.props.height}></canvas>
+      <div ref={(div)=>{this.outterDiv = div;}} className = {this.props.className +" liquid"}>
+        <div ref={(div)=>{this.mainBall = div;}} style={{position:"absolute", backgroundColor:"rgb(65, 244, 217",borderRadius:"50%"}}></div>
+          { smallBubbles}  
       </div>
       )
   }
